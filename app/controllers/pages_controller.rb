664ac4a63c96e4aa8dbf82a707e4c9f8408e3700
Oscar Gay <oscar.gay@icloud.com>
2021-01-16 17:36:31 +0000

    added scraping functionality to include an up-to-date imdb score scraped from the website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
+  require 'open-uri'
+  require 'nokogiri'
   def home
+    @mulholland = scrape("mulholland drive")
+    @eraserhead = scrape("eraserhead")
+    @blue_velvet = scrape("blue velvet")
+    @twin_peaks = scrape("twin peaks fire walk with me")
+    @elephant = scrape("the elephant man")
+  end
+
+  def scrape(film)
+    url = "https://www.imdb.com/find?q=#{film}&ref_=nv_sr_sm"
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    answer = html_doc.search('.result_text').first.css('a')
+    slug = answer.attribute('href').value
+    new_url = "https://www.imdb.com/#{slug}"
+    new_html_file = open(new_url).read
+    new_html_doc = Nokogiri::HTML(new_html_file)
+    rating = new_html_doc.css('.ratingValue').first.css('strong').css('span').css('span').text.strip
+    rating
   end
 end
